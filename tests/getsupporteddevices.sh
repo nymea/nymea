10f008bd2977b97db0a8f71b3d96b70452dafc4c
Michael Zanetti <mzanetti@kde.org>
2014-01-02 01:12:21 +0100

    fix getsupporteddevice script

--- tests/getsupporteddevices.sh
+++ tests/getsupporteddevices.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-(echo '{"id":1, "method":"Devices.GetSupportedDevices"}'; sleep 1) | nc 10.10.10.114 1234
+if [ -z $1 ]; then
+  echo "usage $0 host"
+else
+  (echo '{"id":1, "method":"Devices.GetSupportedDevices"}'; sleep 1) | nc $1 1234
+fi

95f806ccafd3b87ceb2e8727254cfb2b0d6be38f
Simon Stürz <stuerz.simon@gmail.com>
2014-03-31 21:55:33 +0200

    added licence to source code

--- plugins/deviceplugins/intertechno/devicepluginintertechno.h
+++ plugins/deviceplugins/intertechno/devicepluginintertechno.h
@@ -1,3 +1,21 @@
+/****************************************************************************
+ *                                                                          *
+ *  This file is part of guh.                                               *
+ *                                                                          *
+ *  Guh is free software: you can redistribute it and/or modify             *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, version 2 of the License.                 *
+ *                                                                          *
+ *  Guh is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with guh.  If not, see <http://www.gnu.org/licenses/>.            *
+ *                                                                          *
+ ***************************************************************************/
+
 #ifndef DEVICEPLUGININTERTECHNO_H
 #define DEVICEPLUGININTERTECHNO_H
 

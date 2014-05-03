408c115d79bf35df683509ea18ad39c667f263a8
Michael Zanetti <mzanetti@kde.org>
2014-05-04 00:08:10 +0200

    add license headers

--- tests/auto/generatedefines.sh
+++ tests/auto/generatedefines.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
 echo generating defines... $@
-echo \#include \<QMetaType\> > testdefines.h
+
+cat << EOF > testdefines.h 
+/****************************************************************************
+ *                                                                          *
+ *  This file is part of guh.                                               *
+ *                                                                          *
+ *  Guh is free software: you can redistribute it and/or modify             *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, version 2 of the License.                 *
+ *                                                                          *
+ *  Guh is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with guh.  If not, see <http://www.gnu.org/licenses/>.            *
+ *                                                                          *
+ ***************************************************************************/
+
+#include <QMetaType>
+EOF
+
 for i in $@; do
   echo \#include \"${i,,}.h\" >> testdefines.h
 done

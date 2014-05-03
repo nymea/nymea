3bfc17e91e63e0ce77aab23757bccc90a5dd99d8
Michael Zanetti <mzanetti@kde.org>
2014-05-03 02:30:55 +0200

    exclude generated files

--- tests/auto/checklicenseheaders.sh
+++ tests/auto/checklicenseheaders.sh
@@ -1,9 +1,9 @@
-results=`licensecheck -r -c '\.(cpp|h)$' $1 | grep -v "GPL (v2)"`
+results=`licensecheck -r -c '\.(cpp|h)$' $1 | grep -v "GPL (v2)" | grep -v "GENERATED"`
 if test -z "$results"; then
   exit 0
 else
   echo "*** License check failed. Offending files:"
-  licensecheck -r -c '\.(cpp|h)$' $1 | grep -v "GPL (v2)"
+  licensecheck -r -c '\.(cpp|h)$' $1 | grep -v "GPL (v2)" | grep -v "GENERATED"
   exit 1
 fi
 

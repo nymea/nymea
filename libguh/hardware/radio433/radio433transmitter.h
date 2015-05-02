2aa55e469e5bc78dda20601c7ca6c7e3af4c54c9
Simon Stürz <stuerz.simon@gmail.com>
2015-05-02 23:24:24 +0200

    added copyright

--- libguh/hardware/radio433/radio433transmitter.h
+++ libguh/hardware/radio433/radio433transmitter.h
@@ -2,6 +2,8 @@
  *                                                                         *
  *  This file is part of guh.                                              *
  *                                                                         *
+ *  Copyright (C) 2015 Simon Stuerz <simon.stuerz@guh.guru>                *
+ *                                                                         *
  *  Guh is free software: you can redistribute it and/or modify            *
  *  it under the terms of the GNU General Public License as published by   *
  *  the Free Software Foundation, version 2 of the License.                *
--- plugins/deviceplugins/philipshue/light.cpp
+++ plugins/deviceplugins/philipshue/light.cpp
@@ -1,21 +1,23 @@
-/*
- * Copyright 2013 Michael Zanetti
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; version 2.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Authors:
- *      Michael Zanetti <michael_zanetti@gmx.net>
- */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                         *
+ *  Copyright (C) 2013 Michael Zanetti <michael_zanetti@gmx.net>           *
+ *                                                                         *
+ *  This file is part of guh.                                              *
+ *                                                                         *
+ *  Guh is free software: you can redistribute it and/or modify            *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, version 2 of the License.                *
+ *                                                                         *
+ *  Guh is distributed in the hope that it will be useful,                 *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with guh. If not, see <http://www.gnu.org/licenses/>.            *
+ *                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 
 #include "light.h"
 #include "huebridgeconnection.h"

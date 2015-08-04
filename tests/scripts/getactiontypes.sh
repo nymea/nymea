e358fbda7150660ab8315dfb4c7b33279052b184
Simon Stürz <stuerz.simon@gmail.com>
2015-08-04 19:32:21 +0200

    update man page fix test scripts

--- tests/scripts/getactiontypes.sh
+++ tests/scripts/getactiontypes.sh
@@ -3,5 +3,5 @@
 if [ -z $2 ]; then
   echo "usage: $0 host deviceClassId"
 else
-  (echo '{"id":1, "method":"Devices.GetActionTypes", "params":{"deviceClassId":"'$2'"}}'; sleep 1) | nc $1 1234
+  (echo '{"id":1, "method":"Devices.GetActionTypes", "params":{"deviceClassId":"'$2'"}}'; sleep 1) | nc $1 2222
 fi

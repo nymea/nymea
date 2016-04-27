8898d83c86c8e3cd504c3c63a4bb9663dea9adc8
Simon Stürz <stuerz.simon@gmail.com>
2016-04-27 11:25:46 +0200

    update copy right line

--- libguh/hardware/radio433/radio433transmitter.h
+++ libguh/hardware/radio433/radio433transmitter.h
@@ -2,7 +2,7 @@
  *                                                                         *
  *  This file is part of guh.                                              *
  *                                                                         *
- *  Copyright (C) 2015 Simon Stuerz <simon.stuerz@guh.guru>                *
+ *  Copyright (C) 2015 Simon Stürz <simon.stuerz@guh.guru>                 *
  *                                                                         *
  *  Guh is free software: you can redistribute it and/or modify            *
  *  it under the terms of the GNU General Public License as published by   *

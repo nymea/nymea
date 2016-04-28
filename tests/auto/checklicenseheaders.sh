0a2f860c64026f73542188d9f68c7cbcdb4d5afe
Simon Stürz <stuerz.simon@gmail.com>
2016-04-28 14:56:48 +0200

    fix license check and bump guh version

--- tests/auto/checklicenseheaders.sh
+++ tests/auto/checklicenseheaders.sh
@@ -1,9 +1,9 @@
-results=`licensecheck -r -c '\.(cpp|h)$' $1 | grep -v "GPL (v2)" | grep -v "GPL (v3)" | grep -v "BSD" | grep -v "GENERATED"`
+results=`licensecheck -r -c '\.(cpp|h)$' $1 | grep -v "GPL (v2)" | grep -v "GPL (v3)" | grep -v "BSD" | grep -v "GENERATED" | grep -v "LGPL (v2.1 or later)"`
 if test -z "$results"; then
   exit 0
 else
   echo "*** License check failed. Offending files:"
-  licensecheck -r -c '\.(cpp|h)$' $1 | grep -v "GPL (v2)" | grep -v "GPL (v2)" | grep -v "GPL (v3)"  | grep -v "BSD" | grep -v "GENERATED"
+  licensecheck -r -c '\.(cpp|h)$' $1 | grep -v "GPL (v2)" | grep -v "GPL (v2)" | grep -v "GPL (v3)"  | grep -v "BSD" | grep -v "GENERATED" | grep -v "LGPL (v2.1 or later)"
   exit 1
 fi
 

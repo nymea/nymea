eab39db727f696b7e3b04edddc7507c8a30a7245
Simon Stürz <stuerz.simon@gmail.com>
2016-04-29 14:48:49 +0200

    add missing license prepare browsing for plugins

--- libguh/network/avahi/guhavahibrowser.cpp
+++ libguh/network/avahi/guhavahibrowser.cpp
@@ -1,7 +1,44 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                         *
+ *  Copyright (C) 2016 Simon Stürz <simon.stuerz@guh.guru>                 *
+ *                                                                         *
+ *  This file is part of guh.                                              *
+ *                                                                         *
+ *  Guh is free software: you can redistribute it and/or modify            *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, version 2 of the License.                *
+ *                                                                         *
+ *  Guh is distributed in the hope that it will be useful,                 *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with guh. If not, see <http://www.gnu.org/licenses/>.            *
+ *                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include "guhavahibrowser.h"
+#include "loggingcategories.h"
 
 GuhAvahiBrowser::GuhAvahiBrowser(QObject *parent) : QObject(parent)
 {
+    m_browser = new ZConfServiceBrowser(this);
 
+    connect(m_browser, &ZConfServiceBrowser::serviceEntryAdded, this, &GuhAvahiBrowser::onSeviceAdded);
+    connect(m_browser, &ZConfServiceBrowser::serviceEntryRemoved, this, &GuhAvahiBrowser::onSeviceRemoved);
+    connect(m_browser, &ZConfServiceBrowser::serviceBrowsingFinished, this, &GuhAvahiBrowser::onSeviceBrowsingFinished);
+}
+
+void GuhAvahiBrowser::browseService(const QString &serviceType)
+{
+    m_serviceEntries.clear();
+    m_browser->browse(serviceType);
+}
+
+void GuhAvahiBrowser::onSeviceBrowsingFinished()
+{
+
+    emit avahiBrowseFinished(m_serviceEntries);
 }
 
--- libguh/network/avahi/guhavahibrowser.h
+++ libguh/network/avahi/guhavahibrowser.h
@@ -1,7 +1,29 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                         *
+ *  Copyright (C) 2016 Simon Stürz <simon.stuerz@guh.guru>                 *
+ *                                                                         *
+ *  This file is part of guh.                                              *
+ *                                                                         *
+ *  Guh is free software: you can redistribute it and/or modify            *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, version 2 of the License.                *
+ *                                                                         *
+ *  Guh is distributed in the hope that it will be useful,                 *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with guh. If not, see <http://www.gnu.org/licenses/>.            *
+ *                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #ifndef GUHAVAHIBROWSER_H
 #define GUHAVAHIBROWSER_H
 
 #include <QObject>
+
+#include "zconfservicebrowser.h"
 
 class GuhAvahiBrowser : public QObject
 {
@@ -9,9 +31,20 @@ class GuhAvahiBrowser : public QObject
 public:
     explicit GuhAvahiBrowser(QObject *parent = 0);
 
-signals:
+    void browseService(const QString &serviceType);
 
-public slots:
+private:
+    ZConfServiceBrowser *m_browser;
+    QList<AvahiServiceEntry> m_serviceEntries;
+
+signals:
+    void avahiBrowseFinished(const QList<AvahiServiceEntry> &serviceEntries);
+
+private slots:
+    void onSeviceAdded(const QString &name);
+    void onSeviceRemoved(const QString &name);
+    void onSeviceBrowsingFinished();
+
 };
 
 #endif // GUHAVAHIBROWSER_H

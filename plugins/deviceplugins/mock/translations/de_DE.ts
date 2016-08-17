a6dbf36d0ffa73bd9d2c6367906bf5ada1def6cc
Simon Stürz <simon.stuerz@guh.guru>
2016-08-17 17:08:20 +0200

    add translation files to all plugins

--- plugins/deviceplugins/mock/translations/de_DE.ts
+++ plugins/deviceplugins/mock/translations/de_DE.ts
@@ -12,56 +12,56 @@
 <context>
     <name>MockDevice</name>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="50"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="49"/>
         <source>guh</source>
         <extracomment>The name of the vendor (2062d64d-3232-433c-88bc-0d33c0ba2ba6)</extracomment>
         <translation>guh</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="53"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="52"/>
         <source>Mock Device</source>
         <extracomment>The name of the deviceClass</extracomment>
         <translation>Pseudo Gerät</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="56"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="55"/>
         <source>Mock Action 3 (async)</source>
         <extracomment>The name of the ActionType fbae06d3-7666-483e-a39e-ec50fe89054e of deviceClass Mock Device</extracomment>
         <translation>Pseudo Aktion 3 (async)</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="59"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="58"/>
         <source>Mock Action 4 (broken)</source>
         <extracomment>The name of the ActionType df3cf33d-26d5-4577-9132-9823bd33fad0 of deviceClass Mock Device</extracomment>
         <translation>Pseudo Aktion 4 (kaputt)</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="62"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="61"/>
         <source>Mock Action 5 (async, broken)</source>
         <extracomment>The name of the ActionType bfe89a1d-3497-4121-8318-e77c37537219 of deviceClass Mock Device</extracomment>
         <translation>Pseudo Aktion 5 (async, kaputt)</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="65"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="64"/>
         <source>Mock Device (Auto created)</source>
         <extracomment>The name of the deviceClass</extracomment>
         <translation>Pseudo Gerät (Automatisch erstellt)</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="68"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="67"/>
         <source>Mock Device (Push Button)</source>
         <extracomment>The name of the deviceClass</extracomment>
         <translation>Pseudo Gerät (Drückknopf)</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="71"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="70"/>
         <source>Wait 3 second before you continue, the push button will be pressed automatically.</source>
         <extracomment>The pairing info of deviceClass Mock Device (Push Button)</extracomment>
         <translation>Warten Sie 3 Sekunden bevor sie fortfahren, der Knopf wird automatisch für Sie gedrückt.</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="74"/>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="98"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="73"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="97"/>
         <source>Timeout action</source>
         <extracomment>The name of the ActionType 54646e7c-bc54-4895-81a2-590d72d120f9 of deviceClass Mock Device (Push Button)
 ----------
@@ -69,8 +69,8 @@ The name of the ActionType 54646e7c-bc54-4895-81a2-590d72d120f9 of deviceClass M
         <translation>Timeout Aktion</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="77"/>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="101"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="76"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="100"/>
         <source>color</source>
         <extracomment>The name of the stateType (20dc7c22-c50e-42db-837c-2bbced939f8e) of deviceClass Mock Device (Push Button)
 ----------
@@ -78,8 +78,8 @@ The name of the stateType (20dc7c22-c50e-42db-837c-2bbced939f8e) of deviceClass 
         <translation>Farbe</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="80"/>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="104"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="79"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="103"/>
         <source>percentage</source>
         <extracomment>The name of the stateType (72981c04-267a-4ba0-a59e-9921d2f3af9c) of deviceClass Mock Device (Push Button)
 ----------
@@ -87,8 +87,8 @@ The name of the stateType (72981c04-267a-4ba0-a59e-9921d2f3af9c) of deviceClass 
         <translation>Prozent</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="83"/>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="107"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="82"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="106"/>
         <source>allowed values</source>
         <extracomment>The name of the stateType (05f63f9c-f61e-4dcf-ad55-3f13fde2765b) of deviceClass Mock Device (Push Button)
 ----------
@@ -96,8 +96,8 @@ The name of the stateType (05f63f9c-f61e-4dcf-ad55-3f13fde2765b) of deviceClass 
         <translation>Erlaubte Werte</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="86"/>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="110"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="85"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="109"/>
         <source>double value</source>
         <extracomment>The name of the stateType (53cd7c55-49b7-441b-b970-9048f20f0e2c) of deviceClass Mock Device (Push Button)
 ----------
@@ -105,10 +105,10 @@ The name of the stateType (53cd7c55-49b7-441b-b970-9048f20f0e2c) of deviceClass 
         <translation>Double Wert</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="89"/>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="113"/>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="119"/>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="125"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="88"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="112"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="118"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="124"/>
         <source>bool value</source>
         <extracomment>The name of the stateType (e680f7a4-b39e-46da-be41-fa3170fe3768) of deviceClass Mock Device (Push Button)
 ----------
@@ -120,31 +120,31 @@ The name of the stateType (d24ede5f-4064-4898-bb84-cfb533b1fbc0) of deviceClass 
         <translation>Bool Wert</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="92"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="91"/>
         <source>Mock Device (Display Pin)</source>
         <extracomment>The name of the deviceClass</extracomment>
         <translation>Pseudo Gerät (Pin Anzeigen)</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="95"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="94"/>
         <source>Please enter the secret which normaly will be displayed on the device. For the mockdevice the pin is 243681.</source>
         <extracomment>The pairing info of deviceClass Mock Device (Display Pin)</extracomment>
         <translation>Bitte geben sie den Pin ein, der normalerweise auf dem Display des Gerätes angezeigt wird. Für das Pseudo-Gerät lautet der Pin eventType.</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="116"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="115"/>
         <source>Mock Device (Parent)</source>
         <extracomment>The name of the deviceClass</extracomment>
         <translation>Pseudo Gerät (Parent)</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="122"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="121"/>
         <source>Mock Device (Child)</source>
         <extracomment>The name of the deviceClass</extracomment>
         <translation>Pseudo Gerät (Child)</translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="128"/>
+        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/mock/plugininfo.h" line="127"/>
         <source>Mock Device (InputTypes)</source>
         <extracomment>The name of the deviceClass</extracomment>
         <translation>Pseudo Gerät (Eingabe Arten)</translation>

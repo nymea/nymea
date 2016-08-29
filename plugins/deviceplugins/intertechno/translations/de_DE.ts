b53186da1926467dec687bb566273c7ffbded4cd
Simon Stürz <simon.stuerz@guh.io>
2016-08-29 13:01:21 +0200

    translate plugins

--- plugins/deviceplugins/intertechno/translations/de_DE.ts
+++ plugins/deviceplugins/intertechno/translations/de_DE.ts
@@ -10,31 +10,31 @@
         <extracomment>The name of the plugin Intertechno (e998d934-0397-42c1-ad63-9141bcac8563)
 ----------
 The name of the vendor (6a852bc2-34dd-4f4c-9ac9-dd4c32ddbcba)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Intertechno</translation>
     </message>
     <message>
         <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/intertechno/plugininfo.h" line="36"/>
         <source>Intertechno switch</source>
         <extracomment>The name of the DeviceClass (324219e8-7c53-41b5-b314-c2900cd15252)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Intertechno Steckdose</translation>
     </message>
     <message>
         <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/intertechno/plugininfo.h" line="39"/>
         <source>family code</source>
         <extracomment>The name of the paramType (c4e2ec44-5e8e-4168-9f6d-a905ea3329c9) of Intertechno switch</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Familien Kode</translation>
     </message>
     <message>
         <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/intertechno/plugininfo.h" line="42"/>
         <source>button code</source>
         <extracomment>The name of the paramType (abb67f92-2e3b-40e2-9106-e538e1882d11) of Intertechno switch</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Taster Kode</translation>
     </message>
     <message>
         <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/intertechno/plugininfo.h" line="45"/>
         <source>power</source>
         <extracomment>The name of the paramType (8de21063-631c-4419-be5d-235cea3cd906) of Intertechno switch</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>einschalten</translation>
     </message>
 </context>
 </TS>
--- plugins/deviceplugins/udpcommander/translations/de_DE.ts
+++ plugins/deviceplugins/udpcommander/translations/de_DE.ts
@@ -10,31 +10,31 @@
         <extracomment>The name of the plugin UDP Commander (24a8474c-1d86-499e-a76e-9cbfbf48dd72)
 ----------
 The name of the DeviceClass (6ecd5a8d-595a-4520-85e3-dcc9679edf66)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>UDP Kommander</translation>
     </message>
     <message>
         <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/udpcommander/plugininfo.h" line="33"/>
         <source>guh</source>
         <extracomment>The name of the vendor (2062d64d-3232-433c-88bc-0d33c0ba2ba6)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>guh</translation>
     </message>
     <message>
         <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/udpcommander/plugininfo.h" line="39"/>
         <source>port</source>
         <extracomment>The name of the paramType (1843adcb-e377-44d1-8d70-ab4f9eeb32ec) of UDP Commander</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Port</translation>
     </message>
     <message>
         <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/udpcommander/plugininfo.h" line="42"/>
         <source>command</source>
         <extracomment>The name of the paramType (d0f29961-1624-4b91-a0e8-9b1cc86c44c7) of UDP Commander</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Kommando</translation>
     </message>
     <message>
         <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/udpcommander/plugininfo.h" line="45"/>
         <source>command received</source>
         <extracomment>The name of the EventType 5fecbba3-ffbb-456b-872c-a2f571c681cb of deviceClass UDP Commander</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Kommando erhalten</translation>
     </message>
 </context>
 </TS>

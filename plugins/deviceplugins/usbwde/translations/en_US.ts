511f1343d684140bc196e39efe0617de905a87a0
Alexander Lampret <alexander@viracube.com>
2016-10-20 10:08:34 +0200

    translations added

---
+++ plugins/deviceplugins/usbwde/translations/en_US.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>UsbWde</name>
+    <message>
+        <location filename="../../build-usbwde-Desktop-Debug/plugininfo.h" line="42"/>
+        <source>ELV</source>
+        <extracomment>The name of the vendor (55500c7c-9b09-4462-8d71-e5b4b42f6940)</extracomment>
+        <translation>ELV</translation>
+    </message>
+    <message>
+        <location filename="../../build-usbwde-Desktop-Debug/plugininfo.h" line="39"/>
+        <location filename="../../build-usbwde-Desktop-Debug/plugininfo.h" line="45"/>
+        <source>USB WDE</source>
+        <extracomment>The name of the plugin USB WDE (b01bc368-d674-41ef-a368-6b6a76c96365)
+----------
+The name of the DeviceClass (20e1b650-41d1-41b7-891f-1e68130d1403)</extracomment>
+        <translation>USB WDE</translation>
+    </message>
+    <message>
+        <location filename="../../build-usbwde-Desktop-Debug/plugininfo.h" line="48"/>
+        <source>name</source>
+        <extracomment>The name of the paramType (be6b6bd5-9495-45f0-93d7-80f74f633420) of USB WDE</extracomment>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../../build-usbwde-Desktop-Debug/plugininfo.h" line="51"/>
+        <source>interface</source>
+        <extracomment>The name of the paramType (965fd538-8e8a-4a84-97d0-5f8fc9ea115e) of USB WDE</extracomment>
+        <translation>Interface</translation>
+    </message>
+    <message>
+        <location filename="../../build-usbwde-Desktop-Debug/plugininfo.h" line="54"/>
+        <source>baudrate</source>
+        <extracomment>The name of the paramType (b8239c85-8f09-40d5-9ab9-03b3bd0c8fcc) of USB WDE</extracomment>
+        <translation>Baudrate</translation>
+    </message>
+    <message>
+        <location filename="../../build-usbwde-Desktop-Debug/plugininfo.h" line="57"/>
+        <source>Temperature sensor</source>
+        <extracomment>The name of the DeviceClass (e3d839f5-f2a7-48fa-8675-389645413954)</extracomment>
+        <translation>Temperature sensor</translation>
+    </message>
+    <message>
+        <location filename="../../build-usbwde-Desktop-Debug/plugininfo.h" line="60"/>
+        <source>channel</source>
+        <extracomment>The name of the paramType (fc611e60-d975-4d1f-9977-19d2b90c3838) of Temperature sensor</extracomment>
+        <translation>Channel</translation>
+    </message>
+    <message>
+        <location filename="../../build-usbwde-Desktop-Debug/plugininfo.h" line="63"/>
+        <source>Wind/Rain sensor</source>
+        <extracomment>The name of the DeviceClass (2ea29c16-44ad-4cf2-b08d-18c7c6826386)</extracomment>
+        <translation>Wind/Rain sensor</translation>
+    </message>
+</context>
+</TS>
--- plugins/deviceplugins/usbwde/usbwde.pro
+++ plugins/deviceplugins/usbwde/usbwde.pro
@@ -1,4 +1,9 @@
+TRANSLATIONS =  translations/en_US.ts \
+                translations/de_DE.ts
+
 include(../../plugins.pri)
+
+QT += serialport
 
 TARGET = $$qtLibraryTarget(guh_devicepluginusbwde)
 

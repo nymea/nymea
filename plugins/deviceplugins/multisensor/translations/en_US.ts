1f47d00c22e1cf64e1a225bebdfd7ede1768599e
Simon Stürz <simon.stuerz@guh.io>
2016-11-16 15:32:48 +0100

    continue snappy packaging

--- plugins/deviceplugins/multisensor/translations/en_US.ts
+++ plugins/deviceplugins/multisensor/translations/en_US.ts
@@ -4,103 +4,103 @@
 <context>
     <name>MultiSensor</name>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="46"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="46"/>
         <source>MultiSensor</source>
         <extracomment>The name of the plugin MultiSensor (23bf0d8d-df7d-4bee-80ee-f015c5a7f52e)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="49"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="49"/>
         <source>Texas Instruments</source>
         <extracomment>The name of the vendor (2edf543e-dc2c-4693-bb0c-e76c0d305fad)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="52"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="52"/>
         <source>SensorTag</source>
         <extracomment>The name of the DeviceClass (158a06b6-b27f-4951-957e-6f1e3b44f604)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="55"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="55"/>
         <source>name</source>
         <extracomment>The name of the paramType (bdb9d684-2f98-45f5-889c-f184c1e73dc7) of SensorTag</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="58"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="58"/>
         <source>mac address</source>
         <extracomment>The name of the paramType (d51ed68e-c84a-4136-a5b5-be2f95fd5a0f) of SensorTag</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="61"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="61"/>
         <source>available changed</source>
         <extracomment>The name of the autocreated EventType (a9629b11-0f34-47f0-a0f0-f758a6aec2b4)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="64"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="64"/>
         <source>available</source>
         <extracomment>The name of the ParamType of StateType (a9629b11-0f34-47f0-a0f0-f758a6aec2b4) of DeviceClass SensorTag</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="67"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="67"/>
         <source>IR temperature changed</source>
         <extracomment>The name of the autocreated EventType (c664e9ec-a045-49ba-add1-1642ceba7c4f)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="70"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="70"/>
         <source>IR temperature</source>
         <extracomment>The name of the ParamType of StateType (c664e9ec-a045-49ba-add1-1642ceba7c4f) of DeviceClass SensorTag</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="73"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="73"/>
         <source>humidity changed</source>
         <extracomment>The name of the autocreated EventType (e83a50ff-96c9-4b6d-889f-f4238353e794)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="76"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="76"/>
         <source>humidity</source>
         <extracomment>The name of the ParamType of StateType (e83a50ff-96c9-4b6d-889f-f4238353e794) of DeviceClass SensorTag</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="79"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="79"/>
         <source>temperature changed</source>
         <extracomment>The name of the autocreated EventType (8359ada9-df1c-4e60-bb87-9e21d05ee2e2)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="82"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="82"/>
         <source>temperature</source>
         <extracomment>The name of the ParamType of StateType (8359ada9-df1c-4e60-bb87-9e21d05ee2e2) of DeviceClass SensorTag</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="85"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="85"/>
         <source>pressure changed</source>
         <extracomment>The name of the autocreated EventType (645633ad-77d4-45b2-8be8-d6ca7a12eb7a)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="88"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="88"/>
         <source>pressure</source>
         <extracomment>The name of the ParamType of StateType (645633ad-77d4-45b2-8be8-d6ca7a12eb7a) of DeviceClass SensorTag</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="91"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="91"/>
         <source>left key pressed</source>
         <extracomment>The name of the EventType 61478490-bed0-4fed-9d58-f13c35b4f220 of deviceClass SensorTag</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build-guh-Desktop-Debug/plugins/deviceplugins/multisensor/plugininfo.h" line="94"/>
+        <location filename="../../../../../build/plugins/deviceplugins/multisensor/plugininfo.h" line="94"/>
         <source>right key pressed</source>
         <extracomment>The name of the EventType b7e927db-af28-4fdc-8eb7-edb02258ff5a of deviceClass SensorTag</extracomment>
         <translation type="unfinished"></translation>

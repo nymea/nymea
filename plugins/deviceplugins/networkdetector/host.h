a018965ea72158762c7d2ac8bef5627ebf88f204
Simon Stürz <simon.stuerz@guh.io>
2016-11-23 15:43:47 +0100

    upgrade network detector plugin

---
+++ plugins/deviceplugins/networkdetector/host.h
@@ -0,0 +1,47 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                         *
+ *  Copyright (C) 2016 Simon Stürz <simon.stuerz@guh.guru>                 *
+ *                                                                         *
+ *  This file is part of guh.                                              *
+ *                                                                         *
+ *  Guh is free software: you can redistribute it and/or modify            *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, version 2 of the License.                *
+ *                                                                         *
+ *  Guh is distributed in the hope that it will be useful,                 *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with guh. If not, see <http://www.gnu.org/licenses/>.            *
+ *                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef HOST_H
+#define HOST_H
+
+#include <QString>
+#include <QDebug>
+
+class Host
+{
+public:
+    Host();
+    Host(const QString &hostName, const QString &address, const bool &reachable);
+
+    QString hostName() const;
+    QString adderss() const;
+    bool reachable() const;
+
+    bool isValid() const;
+
+private:
+    QString m_hostName;
+    QString m_address;
+    bool m_reachable;
+};
+
+QDebug operator<<(QDebug dbg, const Host &host);
+
+#endif // HOST_H

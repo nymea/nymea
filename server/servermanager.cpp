82c0e0bca627ff029442b918904beadc73ae605b
Michael Zanetti <michael_zanetti@gmx.net>
2017-08-04 01:03:19 +0200

    randomize certificate serial number

--- server/servermanager.cpp
+++ server/servermanager.cpp
@@ -1,6 +1,7 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                                                         *
  *  Copyright (C) 2015 Simon Stürz <simon.stuerz@guh.io>                   *
+ *  Copyright (C) 2017 Michael Zanetti <michael.zanetti@guh.io>            *
  *                                                                         *
  *  This file is part of guh.                                              *
  *                                                                         *

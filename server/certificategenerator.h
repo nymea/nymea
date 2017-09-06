ef5e5e4351a0cbd361c70cfde9a66e746196b6c4
Michael Zanetti <michael_zanetti@gmx.net>
2017-09-06 14:23:18 +0200

    move certificategerneator to a separate file

---
+++ server/certificategenerator.h
@@ -0,0 +1,36 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                         *
+ *  Copyright (C) 2017 Michael Zanetti <michael.zanetti@guh.io>            *
+ *                                                                         *
+ *  This file is part of guh.                                              *
+ *                                                                         *
+ *  Guh is free software: you can redistribute it and/or modify            *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, version 2 of the License.                *
+ *                                                                         *
+ *  Guh is distributed in the hope that it will be useful,                 *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with guh. If not, see <http://www.gnu.org/licenses/>.            *
+ *                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef CERTIFICATEGENERATOR_H
+#define CERTIFICATEGENERATOR_H
+
+#include <QString>
+
+namespace guhserver {
+
+class CertificateGenerator
+{
+public:
+    static void generate(const QString &certificateFilename, const QString &keyFilename);
+};
+
+}
+
+#endif // CERTIFICATEGENERATOR_H

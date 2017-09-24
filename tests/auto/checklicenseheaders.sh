3a20d8396d0c81755dcad96384b1c88f2f14ba05
Michael Zanetti <michael_zanetti@gmx.net>
2017-09-24 22:01:33 +0200

    update licensecheck script to include Apache license

--- tests/auto/checklicenseheaders.sh
+++ tests/auto/checklicenseheaders.sh
@@ -1,9 +1,9 @@
-results=`licensecheck -r -c '\.(cpp|h)$' $1 | grep -v "GPL (v2)" | grep -v "GPL (v3)" | grep -v "BSD" | grep -v "GENERATED" | grep -v "LGPL (v2.1 or later)"`
+results=`licensecheck -r -c '\.(cpp|h)$' $1 | grep -v "GPL (v2)" | grep -v "GPL (v3)" | grep -v "BSD" | grep -v "GENERATED" | grep -v "LGPL (v2.1 or later)" | grep -v "Apache (v2.0)"`
 if test -z "$results"; then
   exit 0
 else
   echo "*** License check failed. Offending files:"
-  licensecheck -r -c '\.(cpp|h)$' $1 | grep -v "GPL (v2)" | grep -v "GPL (v2)" | grep -v "GPL (v3)"  | grep -v "BSD" | grep -v "GENERATED" | grep -v "LGPL (v2.1 or later)"
+  licensecheck -r -c '\.(cpp|h)$' $1 | grep -v "GPL (v2)" | grep -v "GPL (v2)" | grep -v "GPL (v3)"  | grep -v "BSD" | grep -v "GENERATED" | grep -v "LGPL (v2.1 or later)" | grep -v "Apache (v2.0)"
   exit 1
 fi
 

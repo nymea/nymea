a3cb50c0d9db59e4d7f9b7835b82644811d8ca14
Simon Stürz <simon.stuerz@guh.io>
2017-10-10 14:01:51 +0200

    fix testbase and improve settings behaviour

--- tests/auto/settings/testsettings.cpp
+++ tests/auto/settings/testsettings.cpp
@@ -46,54 +46,7 @@ private slots:
 
 void TestSettings::getSetLanguages()
 {
-    enableNotifications();
 
-    QVariant response; QVariantMap params;
-    response = injectAndWait("Configuration.GetAvailableLanguages");
-    QVERIFY2(response.toMap().value("params").toMap().contains("languages"), "Did not get list of languages");
-    QVariantMap responseMap = response.toMap().value("params").toMap();
-    QVERIFY2(responseMap.value("languages").toList().count() >= 2, "Avaliable languages list to short: " +  responseMap.value("languages").toList().count());
-
-    QVariantList languageVariantList = responseMap.value("languages").toList();
-
-
-    QSignalSpy notificationSpy(m_mockTcpServer, SIGNAL(outgoingData(QUuid,QByteArray)));
-
-    foreach (const QVariant &languageVariant, languageVariantList) {
-
-        // Get current configurations
-        response = injectAndWait("Configuration.GetConfigurations");
-        QVariantMap configurationMap = response.toMap().value("params").toMap();
-
-        params.clear();
-        params.insert("language", languageVariant);
-
-        notificationSpy.clear();
-
-        // Set language
-        QVariant response = injectAndWait("Configuration.SetLanguage", params);
-        verifyConfigurationError(response);
-
-        // Check notification
-        notificationSpy.wait(500);
-        QVariantList configurationChangedNotifications = checkNotifications(notificationSpy, "Configuration.LanguageChanged");
-        printJson(configurationChangedNotifications);
-
-        // If the language did not change no notification should be emited
-        if (configurationMap.value("basicConfiguration").toMap().value("language").toString() == languageVariant.toString()) {
-            QVERIFY2(configurationChangedNotifications.count() == 0, "Got Configuration.LanguageChanged notification but should have not.");
-        } else {
-            QVERIFY2(configurationChangedNotifications.count() == 1, "Should get only one Configuration.LanguageChanged notification");
-        }
-    }
-
-    // Reset the language to en_US
-    params.clear(); response.clear();
-    params.insert("language", "en_US");
-
-    // Set language
-    response = injectAndWait("Configuration.SetLanguage", params);
-    verifyConfigurationError(response);
 }
 
 #include "testsettings.moc"

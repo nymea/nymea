2250d694a1012a6fb73f7795090bf06168b59677
Michael Zanetti <michael.zanetti@guh.io>
2018-01-24 22:51:31 +0000

    Added translation using Weblate (Afrikaans)

---
+++ translations/guhd-af.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="af">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../server/main.cpp" line="161"/>
+        <source>
+guh ( /[guːh]/ ) is an open source IoT (Internet of Things) server, 
+which allows to control a lot of different devices from many different 
+manufacturers. With the powerful rule engine you are able to connect any 
+device available in the system and create individual scenes and behaviors 
+for your environment.
+
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../server/main.cpp" line="173"/>
+        <source>Run guhd in the foreground, not as daemon.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../server/main.cpp" line="176"/>
+        <source>Debug categories to enable. Prefix with &quot;No&quot; to disable. Warnings from all categories will be printed unless explicitly muted with &quot;NoWarnings&quot;. 
+
+Categories are:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../server/main.cpp" line="193"/>
+        <source>Enables all debug categories. This parameter overrides all debug category parameters.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../server/main.cpp" line="198"/>
+        <source>Specify a log file to write to, If this option is not specified, logs will be printed to the standard output.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../server/main.cpp" line="201"/>
+        <source>If specified, all D-Bus interfaces will be bound to the session bus instead of the system bus.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../server/main.cpp" line="238"/>
+        <source>No such debug category:</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>

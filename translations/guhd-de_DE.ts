b3a368874d89dc33368d3d25f076a873258c8a39
Jenkins <jenkins@nymea.io>
2018-02-11 23:22:04 +0100

    Automatic translations update by Jenkins

--- translations/guhd-de_DE.ts
+++ translations/guhd-de_DE.ts
@@ -45,24 +45,286 @@ Es gibt folgende Kategorien:</translation>
     </message>
     <message>
         <location filename="../server/main.cpp" line="194"/>
-        <source>Enables all debug categories. This parameter overrides all debug category parameters.</source>
-        <translation>Aktiviere alle Debug-Kategorien. Dieser Parameter überschreibt alle anderen Debug-Kategorien Parameter.</translation>
+        <source>Enables all debug categories. Single debug categories can be disabled again with -d parameter.</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../server/main.cpp" line="199"/>
-        <source>Specify a log file to write to, If this option is not specified, logs will be printed to the standard output.</source>
-        <translation>Geben Sie eine Log Datei an. Wenn diese Option nicht angegeben wird, wird sämtliche Ausgabe auf die Standardausgabe geschrieben.</translation>
+        <location filename="../server/main.cpp" line="197"/>
+        <source>Specify a log file to write to, if this option is not specified, logs will be printed to the standard output.</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../server/main.cpp" line="202"/>
+        <location filename="../server/main.cpp" line="200"/>
         <location filename="../tests/tools/simplepushbuttonhandler/main.cpp" line="33"/>
         <source>If specified, all D-Bus interfaces will be bound to the session bus instead of the system bus.</source>
         <translation>Falls angegeben, werden alle D-Bus Schnittstellen auf dem Sitzungs-Bus anstatt auf dem System-Bus zur verfügung gestellt.</translation>
     </message>
     <message>
-        <location filename="../server/main.cpp" line="239"/>
+        <location filename="../server/main.cpp" line="240"/>
         <source>No such debug category:</source>
         <translation>Diese Debug-Kategorie existiert nicht:</translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="61"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="533"/>
+        <source>Debug nymea</source>
+        <extracomment>The header title of the debug server interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="78"/>
+        <source>nymea debug interface</source>
+        <extracomment>The main title of the debug server interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="87"/>
+        <source>Welcome to the debug interface.</source>
+        <extracomment>The welcome message of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="88"/>
+        <source>This debug interface was designed to provide an easy possibility to get helpful information about the running nymea server.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="104"/>
+        <source>Be aware that this debug interface is a security risk and could offer access to sensible data.</source>
+        <extracomment>The warning message of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="113"/>
+        <source>Server information</source>
+        <extracomment>The server information section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="120"/>
+        <source>User</source>
+        <extracomment>The user name in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="126"/>
+        <source>Compiled with Qt version</source>
+        <extracomment>The Qt build version description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="132"/>
+        <source>Qt runtime version</source>
+        <extracomment>The Qt runtime version description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="138"/>
+        <source>Command</source>
+        <extracomment>The command description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="147"/>
+        <source>Snap name</source>
+        <extracomment>The snap name description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="153"/>
+        <source>Snap version</source>
+        <extracomment>The snap version description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="159"/>
+        <source>Snap directory</source>
+        <extracomment>The snap directory description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="165"/>
+        <source>Snap application data</source>
+        <extracomment>The snap application data description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="171"/>
+        <source>Snap user data</source>
+        <extracomment>The snap user data description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="177"/>
+        <source>Snap common data</source>
+        <extracomment>The snap common data description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="185"/>
+        <source>Server name</source>
+        <extracomment>The server name description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="191"/>
+        <source>Server version</source>
+        <extracomment>The server version description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="197"/>
+        <source>JSON-RPC version</source>
+        <extracomment>The API version description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="203"/>
+        <source>Language</source>
+        <extracomment>The language description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="209"/>
+        <source>Timezone</source>
+        <extracomment>The timezone description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="215"/>
+        <source>Server UUID</source>
+        <extracomment>The server id description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="221"/>
+        <source>Settings path</source>
+        <extracomment>The settings path description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="227"/>
+        <source>Translations path</source>
+        <extracomment>The translation path description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="233"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="271"/>
+        <source>Log database</source>
+        <extracomment>The log database path description in the server infromation section of the debug interface
+----------
+The log databse download description of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="243"/>
+        <source>Plugin paths</source>
+        <extracomment>The plugins path description in the server infromation section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="256"/>
+        <source>Downloads</source>
+        <extracomment>The downloads section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="261"/>
+        <source>Logs</source>
+        <extracomment>The download logs section of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="289"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="321"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="359"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="391"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="423"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="455"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="487"/>
+        <source>Download</source>
+        <extracomment>The download button description of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="304"/>
+        <source>System logs</source>
+        <extracomment>The syslog download description of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="332"/>
+        <source>Settings</source>
+        <extracomment>The settings download section title of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="342"/>
+        <source>Guhd settings</source>
+        <extracomment>The guhd settings download description of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="374"/>
+        <source>Device settings</source>
+        <extracomment>The device settings download description of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="406"/>
+        <source>Device states settings</source>
+        <extracomment>The device states settings download description of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="438"/>
+        <source>Rules settings</source>
+        <extracomment>The rules settings download description of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="470"/>
+        <source>Plugins settings</source>
+        <extracomment>The plugins settings download description of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="502"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="576"/>
+        <source>Released under the GNU GENERAL PUBLIC LICENSE Version 2.</source>
+        <extracomment>The footer license note of the debug interface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="546"/>
+        <source>Error  %1</source>
+        <extracomment>The HTTP error message of the debug interface. The %1 represents the error code ie.e 404</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="653"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="685"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="716"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="745"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="774"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="803"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="832"/>
+        <source>Could not find file &quot;%1&quot;.</source>
+        <extracomment>The HTTP error message of the debug interface. The %1 represents the file name.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="662"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="693"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="724"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="753"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="782"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="811"/>
+        <location filename="../libguh-core/debugserverhandler.cpp" line="840"/>
+        <source>Could not open file &quot;%1&quot;.</source>
+        <extracomment>The HTTP error message of the debug interface. The %1 represents the file name.</extracomment>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>

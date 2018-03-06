b46eea1d611465af49ec0813e0c589a6466eb403
Michael Zanetti <michael_zanetti@gmx.net>
2018-03-06 19:56:57 +0100

    guhTimer -> nymeaTimer

--- libnymea/devicemanager.cpp
+++ libnymea/devicemanager.cpp
@@ -995,7 +995,7 @@ DeviceManager::DeviceError DeviceManager::executeAction(const Action &action)
     return DeviceErrorDeviceNotFound;
 }
 
-/*! Centralized time tick for the GuhTimer resource. Ticks every second. */
+/*! Centralized time tick for the NymeaTimer resource. Ticks every second. */
 void DeviceManager::timeTick()
 {
 

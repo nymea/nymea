37df4f512c4d61307dee15e7d9e47570f97ead23
Michael Zanetti <michael_zanetti@gmx.net>
2018-04-16 21:26:11 +0200

    enable rest for rest tests

--- tests/auto/restdevices/testrestdevices.cpp
+++ tests/auto/restdevices/testrestdevices.cpp
@@ -89,6 +89,7 @@ void TestRestDevices::initTestCase()
     config.address = QHostAddress("127.0.0.1");
     config.port = 3333;
     config.sslEnabled = true;
+    config.restServerEnabled = true;
     NymeaCore::instance()->configuration()->setWebServerConfiguration(config);
     qApp->processEvents();
 }

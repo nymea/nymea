3a88a25c490353ddea13f4627181c8bcbfcc06c9
Michael Zanetti <michael_zanetti@gmx.net>
2019-10-30 12:43:15 +0100

    add copyright headers

--- libnymea/jsonrpc/jsonrpcserver.cpp
+++ libnymea/jsonrpc/jsonrpcserver.cpp
@@ -1,5 +1,25 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                         *
+ *  Copyright (C) 2019 Michael Zanetti <michael.zanetti@nymea.io>          *
+ *                                                                         *
+ *  This file is part of nymea.                                            *
+ *                                                                         *
+ *  nymea is free software: you can redistribute it and/or modify          *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, version 2 of the License.                *
+ *                                                                         *
+ *  nymea is distributed in the hope that it will be useful,               *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with nymea. If not, see <http://www.gnu.org/licenses/>.          *
+ *                                                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include "jsonrpcserver.h"
 
-/*! \fn void JsonRPCServer::registerJsonHandler(JsonHandler *handler)
-    Register a JSON RPC handler on the JSON RPC server.
+/*! \fn void JsonRPCServer::registerExperienceHandler(JsonHandler *handler, int majorVersion, int minorVersion)
+    Register an experience JSON RPC handler on the JSON RPC server.
 */

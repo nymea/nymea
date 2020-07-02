43ed28334057af930dfe528b5d74d4c2c0d30be8
Michael Zanetti <michael_zanetti@gmx.net>
2020-07-02 22:54:00 +0200

    intermediate commit

--- plugins/pymock/integrationpluginpymock.py
+++ plugins/pymock/integrationpluginpymock.py
@@ -9,10 +9,15 @@ def configValueChanged(paramTypeId, value):
     logger.log("Plugin config value changed:", paramTypeId, value)
     if paramTypeId == pyMockPluginAutoThingCountParamTypeId:
         logger.log("Auto Thing Count plugin config changed:", value, "Currently there are:", len(autoThings()), "auto things")
-        for i in range(value, len(myThings())):
+        things = autoThings();
+        for i in range(len(things), value):
             logger.log("Creating new auto thing")
             descriptor = nymea.ThingDescriptor(pyMockAutoThingClassId, "Python Mock auto thing")
             autoThingsAppeared([descriptor])
+
+        for i in range(len(value), things):
+            logger.log("Removing auto thing")
+            autoThingDisappeared(things[i].id)
 
 
 def startMonitoringAutoThings():

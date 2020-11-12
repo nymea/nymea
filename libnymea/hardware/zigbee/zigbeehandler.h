4b493ac51d4a64bd6c838446827a920e9a382948
Michael Zanetti <michael_zanetti@gmx.net>
2020-11-12 11:27:31 +0100

    provisoric handling for node removal

--- libnymea/hardware/zigbee/zigbeehandler.h
+++ libnymea/hardware/zigbee/zigbeehandler.h
@@ -4,7 +4,7 @@
 #include "zigbeenode.h"
 #include "libnymea.h"
 
-class LIBNYMEA_EXPORT ZigbeeHandler
+class LIBNYMEA_EXPORT   ZigbeeHandler
 {
 public:
     ZigbeeHandler();
@@ -12,6 +12,7 @@ public:
 
     virtual QString name() const = 0;
     virtual bool handleNode(ZigbeeNode *node, const QUuid &networkUuid) = 0;
+    virtual void handleRemoveNode(ZigbeeNode *node, const QUuid &networkUuid) = 0;
 };
 
 #endif // ZIGBEEHANDLER_H

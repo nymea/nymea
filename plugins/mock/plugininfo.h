46ccc8a46b9074190efd5a5cc5623c1201f5ae82
Simon Stürz <simon.stuerz@nymea.io>
2024-12-18 10:06:40 +0100

    Update mock plugin info

--- plugins/mock/plugininfo.h
+++ plugins/mock/plugininfo.h
@@ -14,7 +14,7 @@
 #include <QLoggingCategory>
 #include <QObject>
 
-extern "C" const QString libnymea_api_version() { return QString("8.0.0");}
+extern "C" const QString libnymea_api_version() { return QString("9.0.0");}
 
 Q_DECLARE_LOGGING_CATEGORY(dcMock)
 Q_LOGGING_CATEGORY(dcMock, "Mock")
